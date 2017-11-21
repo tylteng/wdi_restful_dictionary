4bfa2b870f1a0615fa39035f3518ea1251fdc7fc
tylteng <teng.ty4@gmail.com>
2017-11-21 17:56:05 -0500

    created view for index method aka entries page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,27 @@
 class EntriesController < ApplicationController
+
+  def index
+    render :index
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
